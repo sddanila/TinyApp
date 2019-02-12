8ecf062068b61177d6c2d95f5ab9015d48ebc358
sddanila <sddanila@yahoo.co.uk>
2019-02-12 22:46:07 +0000

    adding url_index.ejs file with displaying the urls from the express_server.js

--- express_server.js
+++ express_server.js
@@ -13,9 +13,10 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
-});
+app.get("/urls", (req, res) => {
+    let templateVars = { urlDatabase: urlDatabase };
+    res.render("urls_index", templateVars);
+  });
 
 app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
