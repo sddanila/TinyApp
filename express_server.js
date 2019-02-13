1d6289324d98eefcd34f3d88adcccc4149aa6339
sddanila <sddanila@yahoo.co.uk>
2019-02-13 02:38:36 +0000

    add post request route to express_server.js and the from on the urls_new.ejs

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 var PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -18,10 +20,19 @@ app.get("/urls", (req, res) => {
     res.render("urls_index", templateVars);
   });
 
-  app.get("/urls/:shortURL", (req, res) => {
-    let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-    res.render("urls_show", templateVars);
-  });
+app.get("/urls/new", (req, res) => {
+  let templateVars = { urlDatabase: urlDatabase };
+  res.render("urls_new", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  res.render("urls_show");
+});
 
 app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
@@ -30,3 +41,7 @@ app.get("/hello", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+    return Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1);
+}
